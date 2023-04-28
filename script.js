47bc9783890f52f0e20c70efb9bf54ce0d506381
Kauê <kauelinkin@gmail.com>
2023-04-28 09:22:00 -0300

    acesso ao DOM, teste de grid para responsividade

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-let library=[]
+let library=[];
+const title=document.getElementById("title")
+const author=document.getElementById("author")
+const pages=document.getElementById("pages")
+const read= document.getElementById("read")
 
 function book(title,author,pages,read){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.read=read;
-    
-
+    this.title=title.value;
+    this.author=author.value;
+    this.pages=pages.value;
+    this.read=read.value;   
 }
+
 function addBookToLibrary(){
     const newsBook = new book(title,author,pages,read)
     library.push(newsBook)
+}
+function showBooks(){
+    library.forEach(element => {
+        console.log(element)
+        
+    });
+
 }
