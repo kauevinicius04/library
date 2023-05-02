06a356b071b95a23015eb0f5763028754567a8ec
Kauê <kauelinkin@gmail.com>
2023-05-02 13:11:27 -0300

    correcao ao mostrar mensagem de required, ao usar preventDefault no form e adicao de condicao de pagesInput>0 como required

--- script.js
+++ script.js
@@ -1,37 +1,65 @@
-const titleInput=document.getElementById("titleInput")
-const authorInput=document.getElementById("authorInput")
-const pagesInput=document.getElementById("pagesInput")
-const readInput= document.getElementById("readInput")
-const submit=document.getElementById("submit")
-const centerElement=document.getElementById("centered-element")
-const addBook=document.getElementById("addBook")
-const required=document.querySelectorAll("[required]");
+const titleInput = document.getElementById("titleInput")
+const authorInput = document.getElementById("authorInput")
+const pagesInput = document.getElementById("pagesInput")
+const readInput = document.getElementById("readInput")
+const submit = document.getElementById("submit")
+const centerElement = document.getElementById("centered-element")
+const addBook = document.getElementById("addBook")
+const required = document.querySelectorAll("[required]");
+const form= document.getElementsByTagName("form")
 
-const library=[]; 
-function display(displayProperty){
-    centerElement.style.display=displayProperty
+const library = [];
+
+
+function display(displayProperty) {
+    centerElement.style.display = displayProperty
 
 }
-function book(title,author,pages,read){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.read=read;
+
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary(){
-    const newsBook = new book(titleInput.value,authorInput.value,pagesInput.value,readInput.value)
+function addBookToLibrary() {
+    const newsBook = new book(titleInput.value, authorInput.value, pagesInput.value, readInput.value)
     library.push(newsBook)
 }
-addBook.addEventListener("click",()=>{display("grid")})
-submit.addEventListener("click",function a (event){
-    addBookToLibrary()
-    event.preventDefault()})
 
-function showBooks(){
+
+function showBooks() {
     library.forEach(element => {
         console.log(element)
-        
+
     });
 
 }
+
+ const requiring= () => {
+    required.forEach((element) => {
+        if (element.value !== ""&&pagesInput.value>0) {
+            display("none")
+            element.value=""
+         
+           
+        };
+    })
+    };
+  
+
+
+addBook.addEventListener("click", () => {
+    display("grid")
+})
+form[0].addEventListener("submit",(event)=>{
+    event.preventDefault()
+})
+submit.addEventListener("click", () =>{
+    
+    requiring()
+    addBookToLibrary()
+    
+    
+})
