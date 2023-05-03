7679addfaf0539d67432be4723119f5721426f21
Kauê <kauelinkin@gmail.com>
2023-05-03 09:37:48 -0300

    correcao da captura de valor  de readInput, inputs com valor resetado ao clicar em addBook

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const submit = document.getElementById("submit")
 const centerElement = document.getElementById("centered-element")
 const addBook = document.getElementById("addBook")
 const required = document.querySelectorAll("[required]");
+
 const form= document.getElementsByTagName("form")
 
 const library = [];
@@ -24,7 +25,7 @@ function book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-    const newsBook = new book(titleInput.value, authorInput.value, pagesInput.value, readInput.value)
+    const newsBook = new book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked)
     library.push(newsBook)
 }
 
@@ -40,24 +41,29 @@ function showBooks() {
  const requiring= () => {
     required.forEach((element) => {
         if (element.value !== ""&&pagesInput.value>0) {
-            display("none")
-            element.value=""
+            display("none") 
+             form[0].setAttribute('novalidate', '');
+            
+            
          
            
         };
     })
     };
   
-
-
 addBook.addEventListener("click", () => {
+    form[0].removeAttribute("novalidate")
     display("grid")
+    required.forEach(element=>{
+        element.value=""
+    })
+    readInput.checked=false
+    
 })
 form[0].addEventListener("submit",(event)=>{
     event.preventDefault()
 })
 submit.addEventListener("click", () =>{
-    
     requiring()
     addBookToLibrary()
     
