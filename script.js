1c232137879470d1abb3f9a5e25c4fafdff6b047
Kauê <kauelinkin@gmail.com>
2023-05-09 15:30:47 -0300

    adicao dos favicon e comentarios no codigo

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const titleInput = document.querySelector(".title")
+//variaveis de acesso ao DOM
+const titleInput  = document.querySelector(".title")
 const authorInput = document.querySelector(".author")
-const pagesInput = document.querySelector(".pages")
-const readInput = document.querySelector(".read")
-const submit = document.getElementById("submit")
+const pagesInput  = document.querySelector(".pages")
+const readInput   = document.querySelector(".read")
+const submit      =    document.getElementById("submit")
 const centeredElement = document.getElementById("centered-element")
-const addBook = document.getElementById("addBook")
-const required = document.querySelectorAll("[required]");
+const addBook   = document.getElementById("addBook")
+const required  = document.querySelectorAll("[required]");
 const fixedForm = document.getElementById("fixedForm")
-const exit = document.getElementById("exit");
-const overlay = document.getElementById("overlay")
-const library= document.getElementById("library")
-const card = document.getElementsByClassName("card")
+const exit      = document.getElementById("exit");
+const overlay   = document.getElementById("overlay")
+const library   = document.getElementById("library")
+const card      = document.getElementsByClassName("card")
 
+
+//variavel para armazenar os objetos book
 const libraryArray = [];
-let slidedIn = false
-
+// funcao para criar o objeto book
 function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -22,15 +24,44 @@ function book(title, author, pages, read) {
     this.read = read;
 }
 
+
+//criar o objeto book baseado no valor do input e adicionar ao array
 function addBookTolibraryArray() {
     const newsBook = new book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked)
     libraryArray.push(newsBook)
 }
 
+let slidedIn = false
+// funcao para animacao do overlay e do #fixedForm adicionando ou removendo classes que tem os estilos predefinidos no css
+function toggleAnimation() {
+
+     if (slidedIn === false) {
+        centeredElement.classList.add("slide-in")
+        overlay.classList.add("active")
+        addBook.style.transform="scale(0)"
+        slidedIn=true
+        if(centeredElement.classList[0]==="fadeOut"){ 
+        centeredElement.classList.replace("fadeOut","slide-in")
+    }
+
+    }
+       
+    else if(slidedIn===true){
+        centeredElement.classList.replace("slide-in","fadeOut")
+            overlay.style = "initial"
+            overlay.classList.remove("active")
+            addBook.style="initial"
+            slidedIn=false
+    }
+    
+}
+
+
+
 
 
 function showBooks() {
-    // Criar uma cópia do elemento original
+    // Criar uma cópia do elemento #fixedForm
 const cloneCard=fixedForm.cloneNode(true)
 cloneCard.classList = "card"; 
 cloneCard.removeAttribute("id");
@@ -43,7 +74,7 @@ change()
  
   };
 
-
+// funcao para mudar atributos dos clones e adicao de novos atributos
 function change(){
         const lastCloneCard=card[card.length-1];
         const inputOfLastCard=lastCloneCard.querySelectorAll("input");
@@ -66,18 +97,12 @@ function change(){
     
     })
 
-    selectValidation()  
 }
   
 
-// Adicionar o clone modificado a um novo local no DOM
-function selectValidation(){
-    if (fixedForm.hasAttribute("novalidate")) {
-        fixedForm.removeAttribute("novalidate")
-    }
-}
-   
 
+   
+// funcao para checar inputs obrigatorios e caso true mostrar o card do livro
 const requiring = () => {
     let canShowBooks = true;
     // Iterar sobre todos os elementos de entrada
@@ -87,7 +112,7 @@ const requiring = () => {
 
         if (element.hasAttribute('required') && (!element.value || element.value.trim() === '0')) {
     
-            canShowBooks = false; // definir como false se um elemento obrigatório estiver vazio
+            canShowBooks = false; // retornar como false se um elemento obrigatório estiver vazio
         }
     })
     
@@ -99,73 +124,44 @@ const requiring = () => {
     }
 };
 
-addBook.addEventListener("click", addBookReset
-
-)
-exit.addEventListener("click",()=>{
-    toggleAnimation()
-    fixedForm.setAttribute('novalidate', '')
-})
-overlay.addEventListener("click",toggleAnimation
-)
 
 
 
 
 
 
-
-function toggleAnimation() {
-    
-     if (slidedIn === false) {
-        centeredElement.classList.add("slide-in")
-        overlay.classList.add("active")
-        addBook.style.transform="scale(0)"
-        slidedIn=true
-        if(centeredElement.classList[0]==="fadeOut"){ 
-        centeredElement.classList.replace("fadeOut","slide-in")
-    }
-
-    }
-       
-    else if(slidedIn===true){
-        centeredElement.classList.replace("slide-in","fadeOut")
-            overlay.style = "initial"
-            overlay.classList.remove("active")
-            addBook.style="initial"
-            slidedIn=false
-    }
-    
-
-}
-
-
-
-
-
-
+// funcao para retornar os inputs de #fixedForm sem valor ao clicar novamente em #addBook
 function addBookReset() {
     toggleAnimation()
-    
     required.forEach(element => {
         element.value = ""
     })
     readInput.checked = false
     
-
 }
 
+//event listener para adicionar aparicao de #fixedForm
+addBook.addEventListener("click", addBookReset
+
+)
+
+// event listener para sair de #fixedForm
+exit.addEventListener("click",()=>{
+    toggleAnimation()
+    
+})
+
+// event listener para ao clicar no overlay executar animacao e  retornar à tela principal
+overlay.addEventListener("click",toggleAnimation
+)
 
 
-
-
+// eventlistener previnir o submit do form 
 fixedForm.addEventListener("submit", (event) => {
     event.preventDefault()
 })
+
 submit.addEventListener("click", () => {
-    selectValidation()
     requiring()
-
-
 
 })
